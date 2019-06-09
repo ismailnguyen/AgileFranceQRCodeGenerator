b02214dd032d3fff17add3f408e2caf3513c83e6
Ismaïl <nguyen.ismail@gmail.com>
2019-06-10 00:41:28 +0200

    Add QR code generator for Agile France conference

--- AgileFranceQRCodeGenerator/Program.cs
+++ AgileFranceQRCodeGenerator/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json;
 
 namespace AgileFranceQRCodeGenerator
 {
@@ -6,7 +9,64 @@ namespace AgileFranceQRCodeGenerator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Schedule schedule = LoadJson("schedule.json");
+
+            WebClient webClient = new WebClient();
+            foreach (var day in schedule.days)
+            {
+                foreach (var conference in day.events)
+                {
+                    var title = conference.feedbackUrl.Split('-')[1]; // Retrieve id of talk from feedback url (eg. 'https://roti.express/r/AF2019-41' should take only '41')
+                    var fileName = $"{ title }.png";
+
+                    var fileUrl = $"https://api.qrserver.com/v1/create-qr-code/?size=600x600&margin=1&data={ conference.feedbackUrl }";
+
+                    webClient.DownloadFile(fileUrl, fileName);
+
+                    Console.WriteLine($"Downloading { fileName } (from { fileUrl })");
+                }
+            }
+
+            Console.WriteLine($"Downloading finished ! Press any key to exit ...");
+            Console.ReadKey();
+        }
+
+        private static Schedule LoadJson(string fileName)
+        {
+            using (StreamReader reader = new StreamReader(fileName))
+            {
+                string jsonString = reader.ReadToEnd();
+                return JsonConvert.DeserializeObject<Schedule>(jsonString);
+            }
+        }
+
+        public class Schedule
+        {
+            public ScheduleDay[] days;
+        }
+
+        public class ScheduleDay
+        {
+            public string day;
+            public string dayName;
+            public Conference[] events;
+        }
+
+        public class Conference
+        {
+            public int id;
+            public string day;
+            public string dayName;
+            public string halfdayName;
+            public string startTime;
+            public string endTime;
+            public string title;
+            public string description;
+            public string room;
+            public string level;
+            public string objectif;
+            public string[] speakers;
+            public string feedbackUrl;
         }
     }
 }
